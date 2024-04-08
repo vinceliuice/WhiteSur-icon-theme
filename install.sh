9741dacf67998ac203bf9b073e41df31dd22cf58
vinceliuice <vinceliuice@hotmail.com>
2024-04-09 00:56:16 +0800

    Fixed issues

--- install.sh
+++ install.sh
@@ -124,7 +124,7 @@ install() {
     cp -r "${SRC_DIR}"/src/categories/symbolic                                               "${THEME_DIR}"/categories
     cp -r "${SRC_DIR}"/src/emblems/symbolic                                                  "${THEME_DIR}"/emblems
     cp -r "${SRC_DIR}"/src/mimes/symbolic                                                    "${THEME_DIR}"/mimes
-    cp -r "${SRC_DIR}"/src/devices/{16,22,24,symbolic}                                       "${THEME_DIR}"/devices
+    cp -r "${SRC_DIR}"/src/devices/{16,22,24,32,symbolic}                                    "${THEME_DIR}"/devices
     cp -r "${SRC_DIR}"/src/places/{16,22,24,scalable,symbolic}                               "${THEME_DIR}"/places
     cp -r "${SRC_DIR}"/src/status/symbolic                                                   "${THEME_DIR}"/status
 
@@ -145,11 +145,11 @@ install() {
 
     # Change icon color for dark theme
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,devices,places}/{16,22,24}/*
-    sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/actions/32/*
+    sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,devices}/32/*
     sed -i "s/#363636/#dedede/g" "${THEME_DIR}"/{actions,apps,categories,emblems,devices,mimes,places,status}/symbolic/*
 
     cp -r "${SRC_DIR}"/links/actions/{16,22,24,32,symbolic}                                  "${THEME_DIR}"/actions
-    cp -r "${SRC_DIR}"/links/devices/{16,22,24,symbolic}                                     "${THEME_DIR}"/devices
+    cp -r "${SRC_DIR}"/links/devices/{16,22,24,32,symbolic}                                  "${THEME_DIR}"/devices
     cp -r "${SRC_DIR}"/links/places/{16,22,24,scalable,symbolic}                             "${THEME_DIR}"/places
     cp -r "${SRC_DIR}"/links/status/symbolic                                                 "${THEME_DIR}"/status
     cp -r "${SRC_DIR}"/links/apps/symbolic                                                   "${THEME_DIR}"/apps
